fefa945814990bf3563cf42417d0fa2ac25b2559
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-11 23:41:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Python List Comprehension Error Handling:
+# The correct responses to the question "how to handle exceptions in a list 
+# comprehension" are all expressing part of all of this truth: 1) literally, 
+# i.e. lexically IN the comprehension itself, you can't; 2) practically, you 
+# delegate the job to a function or check for error prone values when that's 
+# feasible.
